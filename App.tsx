1d819cb21ef223298529e6ff9315e77b773d7cb2
vanor-cardozo <vanor.fc@gmail.com>
2023-05-19 14:38:56 -0300

    feat: Configuração para fonte e paleta de cores

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>ToDo List</Text>
